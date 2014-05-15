4aba18cd84283501fe3453684e1924370b78e1b9
Ash Wilson <smashwilson@gmail.com>
2014-05-15 15:42:32 -0400

    Start with a basic app.js.

---
+++ _assets/javascripts/app.js
@@ -0,0 +1,7 @@
+// This is the master, all-in-one JavaScript file that's concatenated from the
+// others under this directory. Rather than adding JavaScript here, add it
+// somewhere else in this directory and it'll be included.
+
+//= require jquery
+//= require bootstrap
+//= require_tree .
