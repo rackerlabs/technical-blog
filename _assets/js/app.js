3414264abc752fbe89121df1423db6f81533f7a4
rdodev <rubenoz@gmail.com>
2014-07-10 12:22:32 -0400

    - Removed explicit GA code from app.ja init  - added script tealium js tag to default layout

--- _assets/js/app.js
+++ _assets/js/app.js
@@ -46,19 +46,6 @@
 
     window.hljs.initHighlightingOnLoad();
 
-    var _gaq = window._gaq || [];
-    _gaq.push(['_setAccount', 'UA-35639070-1']);
-    _gaq.push(['_setDomainName', 'rackspace.com']);
-    _gaq.push(['_trackPageview']);
-
-    (function () {
-      var ga = document.createElement('script');
-      ga.type = 'text/javascript';
-      ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0];
-      s.parentNode.insertBefore(ga, s);
-    })();
 
     app.routes = {
       '/community/': app.pages.sponsorship,
