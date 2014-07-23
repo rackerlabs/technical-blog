62163ce6cd6af57f58f2a63d8b854ceb1b249d1d
Ken Perkins <ken.perkins@rackspace.com>
2014-07-22 20:21:00 -0700

    Fixing more and youtube in blog post
    - fixes #698

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -7,7 +7,7 @@ module Jekyll
     end
 
     def render(context)
-      %(<div class="embed-video-container"><iframe src="http://www.youtube.com/embed/#{@id}"></iframe></div>)
+      %(<div class="embed-video-container"><iframe src="//www.youtube.com/embed/#{@id}"></iframe></div>)
     end
   end
 end
