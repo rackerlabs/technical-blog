d4d5c8321f2c1536292bbf12da461b7541151152
Ravi <raviteja.valluri@rackspace.com>
2020-10-29 14:58:08 -0700

    Crawler for search

--- src/js/app.js
+++ src/js/app.js
@@ -10,12 +10,12 @@ contentLoaded().then(() => {
   try {
     let lastRenderArgs;
     let renderHTML = ``;
-    // algoliasearchNetlify({
-    //   appId: ALGOLIA_NETLIFY_APP_ID,
-    //   apiKey: ALGOLIA_NETLIFY_SEARCH_KEY,
-    //   siteId: ALGOLIA_SITE_ID,
-    //   branch: ALGOLIA_NETLIFY_BRANCH,
-    // });
+    algoliasearchNetlify({
+      appId: ALGOLIA_NETLIFY_BLOG_APP_ID,
+      apiKey: ALGOLIA_NETLIFY_BLOG_SEARCH_KEY,
+      siteId: ALGOLIA_SITE_ID,
+      branch: ALGOLIA_NETLIFY_BRANCH
+    });
     const infiniteHits = instantsearch.connectors.connectInfiniteHits(
       (renderArgs, isFirstRender) => {
         const {
@@ -45,42 +45,123 @@ contentLoaded().then(() => {
           .map(
             (hit) => {
               const entities = new Entities();
-              if (hit.categories != null && hit.date != null && hit.date != '' && hit.url != null && hit.author != null) {
-                renderHTML = `<li class="hit-item-single">
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <p class="search-product">
-                        <a class="search-product-link" href="/blog/">Blog</a> &nbsp; > &nbsp; <a class="search-product-link" href=/blog/${hit.categories}>${hit.categories}</a>
-                      </p> 
-                      <h2>
-                        <a class="search-title" href=/blog/${hit.url}>${instantsearch.highlight({ attribute: 'title', hit })}</a>
-                      </h2>
-                      <a class="search-summary-link" href=/blog/${hit.url}>
-                        <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
-                      </a>
-                      <span class="search-author" > By &nbsp; ${instantsearch.highlight({ attribute: 'author', hit })}</span>
-                      <span class="search-date">${moment(hit.date).format('LL')}</span> 
-                  </div>
-                </div>
-              </li>`;
-              } else if (hit.product_url != null && hit.created_by != null && hit.created_by != '' && hit.created_date != null && !hit.permalink.includes('all-articles')) {
-                renderHTML = `<li class="hit-item-single">
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <p class="search-product">
-                        <a class="search-product-link" href="/support/how-to/">How-To</a> &nbsp; > &nbsp; <a class="search-product-link" href=/support/how-to/${hit.product_url}>${hit.product}</a>
-                      </p> 
-                      <h2>
-                        <a class="search-title" href=/support/how-to/${hit.permalink}>${instantsearch.highlight({ attribute: 'title', hit })}</a>
-                      </h2>
-                      <a class="search-summary-link" href=/support/how-to/${hit.permalink}>
-                        <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
-                      </a>
-                      <span class="search-author" > By &nbsp; ${instantsearch.highlight({ attribute: 'created_by', hit })}</span>
-                      <span class="search-date">${hit.last_modified_date}</span> 
-                  </div>
-                </div>
-              </li>`;
+              if (hit) {
+              //   renderHTML = `<li class="hit-item-single">
+              //     <div class="row">
+              //       <div class="col-sm-12">
+              //         <p class="search-product">
+              //           <a class="search-product-link" href="/blog/">Blog</a> &nbsp; > &nbsp; <a class="search-product-link" href=/blog/${hit.categories}>${hit.categories}</a>
+              //         </p> 
+              //         <h2>
+              //           <a class="search-title" href=/blog/${hit.url}>${instantsearch.highlight({ attribute: 'title', hit })}</a>
+              //         </h2>
+              //         <a class="search-summary-link" href=/blog/${hit.url}>
+              //           <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
+              //         </a>
+              //         <span class="search-author" > By &nbsp; ${instantsearch.highlight({ attribute: 'author', hit })}</span>
+              //         <span class="search-date">${moment(hit.date).format('LL')}</span> 
+              //     </div>
+              //   </div>
+              // </li>`;
+              renderHTML = `<li class="hit-item-single">
+              <div class="row">
+                <div class="col-sm-12">
+                    <a class="search-title">${instantsearch.highlight({ attribute: 'title', hit })}</a>
+                  </h2>
+                  <a class="search-summary-link">
+                    <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
+                  </a>
+              </div>
+            </div>
+          </li>`;
+              } 
+              // else if (hit.product_url != null && hit.created_by != null && hit.created_by != '' && hit.created_date != null && !hit.permalink.includes('all-articles')) {
+              //   renderHTML = `<li class="hit-item-single">
+              //     <div class="row">
+              //       <div class="col-sm-12">
+              //         <p class="search-product">
+              //           <a class="search-product-link" href="/support/how-to/">How-To</a> &nbsp; > &nbsp; <a class="search-product-link" href=/support/how-to/${hit.product_url}>${hit.product}</a>
+              //         </p> 
+              //         <h2>
+              //           <a class="search-title" href=/support/how-to/${hit.permalink}>${instantsearch.highlight({ attribute: 'title', hit })}</a>
+              //         </h2>
+              //         <a class="search-summary-link" href=/support/how-to/${hit.permalink}>
+              //           <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
+              //         </a>
+              //         <span class="search-author" > By &nbsp; ${instantsearch.highlight({ attribute: 'created_by', hit })}</span>
+              //         <span class="search-date">${hit.last_modified_date}</span> 
+              //     </div>
+              //   </div>
+              // </li>`;
+                // } else 
+                else {
+                renderHTML = `<span></span>`
+              }
+              return entities.decode(renderHTML);
+            }
+          )
+          .join('');
+      }
+    );
+    const supportInfiniteHits = instantsearch.connectors.connectInfiniteHits(
+      (renderArgs, isFirstRender) => {
+        const {
+          hits,
+          showMore,
+          widgetParams
+        } = renderArgs;
+        const {
+          container
+        } = widgetParams;
+        lastRenderArgs = renderArgs;
+        if (isFirstRender) {
+          const sentinel = document.createElement('div');
+          container.appendChild(document.createElement('ul'));
+          container.appendChild(sentinel);
+          const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+              if (entry.isIntersecting && !lastRenderArgs.isLastPage) {
+                showMore();
+              }
+            });
+          });
+          observer.observe(sentinel);
+          return;
+        }
+        container.querySelector('ul').innerHTML = hits
+          .map(
+            (hit) => {
+              const entities = new Entities();
+              if (hit) {
+              //   renderHTML = `<li class="hit-item-single">
+              //     <div class="row">
+              //       <div class="col-sm-12">
+              //         <p class="search-product">
+              //           <a class="search-product-link" href="/support/how-to/">How-To</a> &nbsp; > &nbsp; <a class="search-product-link" href=/support/how-to/${hit.product_url}>${hit.product}</a>
+              //         </p> 
+              //         <h2>
+              //           <a class="search-title" href=/support/how-to/${hit.permalink}>${instantsearch.highlight({ attribute: 'title', hit })}</a>
+              //         </h2>
+              //         <a class="search-summary-link" href=/support/how-to/${hit.permalink}>
+              //           <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
+              //         </a>
+              //         <span class="search-author" > By &nbsp; ${instantsearch.highlight({ attribute: 'created_by', hit })}</span>
+              //         <span class="search-date">${hit.last_modified_date}</span> 
+              //     </div>
+              //   </div>
+              // </li>`;
+              renderHTML = `<li class="hit-item-single">
+              <div class="row">
+                <div class="col-sm-12">
+                  <h2>
+                    <a class="search-title">${instantsearch.highlight({ attribute: 'title', hit })}</a>
+                  </h2>
+                  <a class="search-summary-link">
+                    <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
+                  </a>
+              </div>
+            </div>
+          </li>`;
               } else {
                 renderHTML = `<span></span>`
               }
@@ -90,62 +171,6 @@ contentLoaded().then(() => {
           .join('');
       }
     );
-    // const supportInfiniteHits = instantsearch.connectors.connectInfiniteHits(
-    //   (renderArgs, isFirstRender) => {
-    //     const {
-    //       hits,
-    //       showMore,
-    //       widgetParams
-    //     } = renderArgs;
-    //     const {
-    //       container
-    //     } = widgetParams;
-    //     lastRenderArgs = renderArgs;
-    //     if (isFirstRender) {
-    //       const sentinel = document.createElement('div');
-    //       container.appendChild(document.createElement('ul'));
-    //       container.appendChild(sentinel);
-    //       const observer = new IntersectionObserver(entries => {
-    //         entries.forEach(entry => {
-    //           if (entry.isIntersecting && !lastRenderArgs.isLastPage) {
-    //             showMore();
-    //           }
-    //         });
-    //       });
-    //       observer.observe(sentinel);
-    //       return;
-    //     }
-    //     container.querySelector('ul').innerHTML = hits
-    //       .map(
-    //         (hit) => {
-    //           const entities = new Entities();
-    //           if (hit.product_url != null && hit.created_by != null && hit.created_by != '' && hit.created_date != null && !hit.permalink.includes('all-articles')) {
-    //             renderHTML = `<li class="hit-item-single">
-    //               <div class="row">
-    //                 <div class="col-sm-12">
-    //                   <p class="search-product">
-    //                     <a class="search-product-link" href="/support/how-to/">How-To</a> &nbsp; > &nbsp; <a class="search-product-link" href=/support/how-to/${hit.product_url}>${hit.product}</a>
-    //                   </p> 
-    //                   <h2>
-    //                     <a class="search-title" href=/support/how-to/${hit.permalink}>${instantsearch.highlight({ attribute: 'title', hit })}</a>
-    //                   </h2>
-    //                   <a class="search-summary-link" href=/support/how-to/${hit.permalink}>
-    //                     <p class="search-summary">${instantsearch.highlight({ attribute: 'content', hit })}</p>
-    //                   </a>
-    //                   <span class="search-author" > By &nbsp; ${instantsearch.highlight({ attribute: 'created_by', hit })}</span>
-    //                   <span class="search-date">${hit.last_modified_date}</span> 
-    //               </div>
-    //             </div>
-    //           </li>`;
-    //           } else {
-    //             renderHTML = `<span></span>`
-    //           }
-    //           return entities.decode(renderHTML);
-    //         }
-    //       )
-    //       .join('');
-    //   }
-    // );
     const renderStats = (renderOptions, isFirstRender) => {
       const {
         nbHits,
@@ -167,7 +192,7 @@ contentLoaded().then(() => {
       }
       widgetParams.container.innerHTML = `${count}`;
     };
-    const algoliaClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_KEY);
+    const algoliaClient = algoliasearch(ALGOLIA_NETLIFY_BLOG_APP_ID, ALGOLIA_NETLIFY_BLOG_SEARCH_KEY);
     const searchClient = {
       search(requests) {
         if (requests.every(({
@@ -187,14 +212,13 @@ contentLoaded().then(() => {
     // Create the custom widget
     const customStats = instantsearch.connectors.connectStats(renderStats);
     const search = instantsearch({
-      indexName: ALGOLIA_PUBLIC_INDEX,
+      indexName: ALGOLIA_NETLIFY_BLOG_INDEX,
       searchClient: searchClient,
       searchFunction(helper) {
         const hitsContainer = document.querySelector('#hits');
         const paginationContainer = document.querySelector('#pagination');
         const statsContainer = document.querySelector('#stats');
         hitsContainer.style.display = helper.state.query === '' ? 'none' : '';
-        
         paginationContainer.style.display = helper.state.query === '' ? 'none' : '';
         statsContainer.style.display = helper.state.query === '' ? 'none' : '';
         helper.search();
@@ -223,13 +247,6 @@ contentLoaded().then(() => {
           '*'
         ]
       }),
-      // instantsearch.widgets.index({
-      //   indexName: AlGOLIA_SUPPORT_INDEX
-      // }).addWidgets([
-      //   supportInfiniteHits({
-      //     container: document.querySelector('#support-hits')
-      //   }),
-      // ]),
       infiniteHits({
         container: document.querySelector('#hits')
       }),
--- webpack.config.js
+++ webpack.config.js
@@ -39,14 +39,18 @@ module.exports = {
       // Provide enviroment variable defaults
       // from .env
       ALGOLIA_APP_ID: JSON.stringify(process.env.ALGOLIA_APP_ID),
-      ALGOLIA_NETLIFY_APP_ID: JSON.stringify(process.env.ALGOLIA_NETLIFY_APP_ID),
+      ALGOLIA_NETLIFY_BLOG_APP_ID: JSON.stringify(process.env.ALGOLIA_NETLIFY_BLOG_APP_ID),
+      ALGOLIA_NETLIFY_SUPPORT_APP_ID: JSON.stringify(process.env.ALGOLIA_NETLIFY_SUPPORT_APP_ID),
       ALGOLIA_SEARCH_KEY: JSON.stringify(process.env.ALGOLIA_SEARCH_KEY),
-      ALGOLIA_NETLIFY_SEARCH_KEY: JSON.stringify(process.env.ALGOLIA_NETLIFY_SEARCH_KEY),
+      ALGOLIA_NETLIFY_BLOG_SEARCH_KEY: JSON.stringify(process.env.ALGOLIA_NETLIFY_BLOG_SEARCH_KEY),
+      ALGOLIA_NETLIFY_SUPPORT_SEARCH_KEY: JSON.stringify(process.env.ALGOLIA_NETLIFY_SUPPORT_SEARCH_KEY),
       ALGOLIA_BLOG_INDEX: JSON.stringify(process.env.ALGOLIA_BLOG_INDEX),
       ALGOLIA_SUPPORT_INDEX: JSON.stringify(process.env.ALGOLIA_SUPPORT_INDEX),
       ALGOLIA_PUBLIC_INDEX: JSON.stringify(process.env.ALGOLIA_PUBLIC_INDEX),
-      ALGOLIA_NETLIFY_INDEX: JSON.stringify(process.env.ALGOLIA_NETLIFY_INDEX),
+      ALGOLIA_NETLIFY_BLOG_INDEX: JSON.stringify(process.env.ALGOLIA_NETLIFY_BLOG_INDEX),
+      ALGOLIA_NETLIFY_SUPPORT_INDEX: JSON.stringify(process.env.ALGOLIA_NETLIFY_SUPPORT_INDEX),
       ALGOLIA_ADMIN_KEY: JSON.stringify(process.env.ALGOLIA_ADMIN_KEY),
-      ALGOLIA_SITE_ID: JSON.stringify(process.env.ALGOLIA_SITE_ID)
+      ALGOLIA_SITE_ID: JSON.stringify(process.env.ALGOLIA_SITE_ID),
+      ALGOLIA_NETLIFY_BRANCH: JSON.stringify(process.env.ALGOLIA_NETLIFY_BRANCH)
     })],
 };
