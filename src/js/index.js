5bbceb4d317ea00e6ea0db22c846f591ffc41043
Ravi <raviteja.valluri@rackspace.com>
2020-11-04 15:57:24 -0800

    New Crawler Plugin Install/Uninstall

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,3 @@ import ReactDOM from 'react-dom';
 import App from './imports/App';
 
 ReactDOM.render(<App />, document.getElementById('search'));
-
-contentLoaded().then(() => {
-    algoliasearchNetlify({
-        appId: 'LMBSSCOJZ7',
-        apiKey: '1a804c00fea1ade94eaf7753cba59a2f',
-        siteId: '24f76c9d-025f-4764-aed0-ee8531de9936',
-        branch: 'staging',
-      });
- });
