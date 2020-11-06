9e67ff48c16d8f7ffc3e98d03bded3968aab42bc
Ravi <raviteja.valluri@rackspace.com>
2020-11-06 14:19:28 -0800

    Fix UI for new indices

--- src/js/imports/Hit.js
+++ src/js/imports/Hit.js
@@ -8,22 +8,16 @@ const Hit = ({ hit }) => (
   <div className="row">
     <div className="col-sm-12">
         <a className="search-type-link" href="/blog">Blog</a>&nbsp;&gt;&nbsp;
-          <ul>{
-          hit.categories.map(function(item, index) {
-            return <li key={`${index}`}><a className="search-type-link">{ (index ? ', ' : '') + item }</a></li>;
-          })
-        }</ul>
+        <a className="search-type-link">{hit.category}</a>
       <h2>
       <a className="search-title-link" href={`/support/how-to/${hit.url}`}>
         <Highlight attribute="title" hit={hit} />
       </a>
       </h2>
       <a className="search-summary-link" href={`/support/how-to/${hit.url}`}>
-        <p className="search-summary"><Highlight attribute="summary" hit={hit} /></p>
+        <p className="search-summary"><Highlight attribute="description" hit={hit} /></p>
       </a>
-      <span className="search-author" > By &nbsp; { hit.authors.map(function(item, index) {
-        return <li key={`${index}`}><a className="search-author-link" href={`/support/how-to/${item}`}>{ (index ? ', ' : '') + item }</a></li>;
-          })}</span>
+      <span className="search-author" > By &nbsp; <a className="search-author-link" href={`/support/how-to/${hit.authors}`}><Highlight attribute="authors" hit={hit} tagName="mark" /></a></span>
       <span className="search-date">{moment(hit.datePublished).format('LL')}</span>
     </div>
   </div>
