71e55728bde368c5eac9e19ed72f818947ece6f3
Ravi <raviteja.valluri@rackspace.com>
2020-11-10 10:12:54 -0800

    UI for atomic-algoliaD

--- src/js/imports/Hit.js
+++ src/js/imports/Hit.js
@@ -9,11 +9,12 @@ import { Snippet } from 'react-instantsearch-dom';
 const Hit = ({ hit }) => {
   const entities = new Entities();
   if (hit.categories != null && hit.date != null && hit.date != '' && hit.url != null && hit.author != null) {
+    hit.categories.map(category => console.log(category));
     return (
       <div className="row">
         <div className="col-sm-12">
           <a className="search-type-link" href="/blog">Blog</a>&nbsp;&gt;&nbsp;
-          <a className="search-type-link" href={`/blog/${hit.categories}`}>{hit.categories}</a>
+          {hit.categories.map((category, index) => <a key={category} href={`/blog/categories/${category.trimStart().replace(/\s+/g, '-')}`} className="search-type-link">{category} {index < hit.categories.length - 1 ? ',\u00A0' : ''}</a>)}
           <h2>
             <a className="search-title-link" href={`/blog/${hit.url}`}>
               <Highlight attribute="title" hit={hit} />
@@ -22,7 +23,7 @@ const Hit = ({ hit }) => {
           <a className="search-summary-link" href={`/blog/${hit.url}`}>
             <p className="search-summary"><Snippet hit={hit} attribute="content" tagName="mark"/></p>
           </a>
-          <span className="search-author" > By &nbsp; <a className="search-author-link" href={`/blog/authors/${hit.author}`}>{hit.author}</a></span>
+          <span className="search-author" > By &nbsp; <a className="search-author-link" href={`/blog/authors/${hit.author.replace(/\s+/g, '-')}`}>{hit.author}</a></span>
           <span className="search-date">{moment(hit.date).format('LL')}</span>
         </div>
       </div>
