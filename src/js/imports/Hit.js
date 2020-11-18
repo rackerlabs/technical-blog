14389a2bdabd67ae3a692b39a8bbaa08e2d6d39b
Ravi <raviteja.valluri@rackspace.com>
2020-11-18 09:04:17 -0800

    Bug Fixes

--- src/js/imports/Hit.js
+++ src/js/imports/Hit.js
@@ -3,7 +3,7 @@ import Highlight from './Highlight';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 const Entities = require('html-entities').AllHtmlEntities;
-import { Snippet } from 'react-instantsearch-dom';
+import Snippet from './Snippet';
 
 
 const Hit = ({ hit }) => {
@@ -15,11 +15,11 @@ const Hit = ({ hit }) => {
           <a className="search-type-link" href="/blog">Blog</a>&nbsp;&gt;&nbsp;
           {hit.categories.map((category, index) => <a key={category} href={`/blog/categories/${category.trimStart().replace(/\s+/g, '-')}`} className="search-type-link">{category} {index < hit.categories.length - 1 ? ',\u00A0' : ''}</a>)}
           <h2>
-            <a className="search-title-link" href={`/blog/${hit.url}`}>
+            <a className="search-title-link" href={`/blog/${hit.url.replace(/\s+/g, '-')}`}>
               <Highlight attribute="title" hit={hit} />
             </a>
           </h2>
-          <a className="search-summary-link" href={`/blog/${hit.url}`}>
+          <a className="search-summary-link" href={`/blog/${hit.url.replace(/\s+/g, '-')}`}>
             <p className="search-summary"><Snippet hit={hit} attribute="content" tagName="mark"/></p>
           </a>
           <span className="search-author" > By &nbsp; <a className="search-author-link" href={`/blog/authors/${hit.author.replace(/\s+/g, '-')}`}><Highlight attribute="author" hit={hit} /></a></span>
